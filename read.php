242d883bc2b1ebd1c61e10f208124e0737a9f2fe
satishNunakana <76905574+satishNunakana@users.noreply.github.com>
2021-07-05 15:25:29 +0530

    modified the table structure.

--- read.php
+++ read.php
@@ -69,18 +69,42 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
             <div class="row">
                 <div class="col-md-12">
                     <h1 class="mt-5 mb-3">View Record</h1>
-                    <div class="form-group">
+                    <table  class="table table-bordered">
+                        <tr>
+                            <td>
+                                 <label>Name</label>
+                            </td>
+                            <td>
+                                 <label>Address</label>
+                            </td>
+                            <td>
+                                <label>Salary</label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p><b><?php echo $row["name"]; ?></b></p>
+                            </td>
+                            <td>
+                                 <p><b><?php echo $row["address"]; ?></b></p>
+                            </td>
+                            <td>
+                                 <p><b><?php echo $row["salary"]; ?></b></p>
+                            </td>
+                        </tr>
+                    </table>
+                    <!-- <div class="form-group">
                         <label>Name</label>
                         <p><b><?php echo $row["name"]; ?></b></p>
-                    </div>
-                    <div class="form-group">
+                    </div> -->
+                    <!-- <div class="form-group">
                         <label>Address</label>
                         <p><b><?php echo $row["address"]; ?></b></p>
                     </div>
                     <div class="form-group">
                         <label>Salary</label>
                         <p><b><?php echo $row["salary"]; ?></b></p>
-                    </div>
+                    </div> -->
                     <p><a href="index.php" class="btn btn-primary">Back</a></p>
                 </div>
             </div>        
